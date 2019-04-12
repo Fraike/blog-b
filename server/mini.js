9248e05bea9391ed217afa3d3e15ef0cf2e864d8
戴扬杰 <1318610785@qq.com>
2019-04-12 15:50:25 +0800

    update

--- server/mini.js
+++ server/mini.js
@@ -7,22 +7,22 @@ const mShare = model.getModel('mshare')
 Router.post('/uploadMShare',function(req,res){
     const {title,content,date,imagesUrl,time,location} = req.body
     console.log(req.body)
-    const mShareModel = new mShare({title,content,date,imagesUrl,time,location})
-    mShareModel.save(function(e,d){
-        if(e){
-            return res.json({code:1,msg:'后端出错'})
-        }
-        return res.json({code:0,msg:'success'})
-    })
+    // const mShareModel = new mShare({title,content,date,imagesUrl,time,location})
+    // mShareModel.save(function(e,d){
+    //     if(e){
+    //         return res.json({code:1,msg:'后端出错'})
+    //     }
+    //     return res.json({code:0,msg:'success'})
+    // })
    
 })
 
 Router.get('/getAllMShare',function(req,res){
     mShare.find({},function(err,doc){
         if (err) {
-            return res.json({code:1,msg:'获取文章列表失败'})
+            return res.json({code:1,msg:'获取小程序首页数据成功'})
         }
-        return res.json({code:0,list:doc,msg:'获取文章列表成功'})
+        return res.json({code:0,list:doc,msg:'获取小程序首页数据失败'})
     })
    
 })
