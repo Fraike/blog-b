ca3d8e75c8105514f12d425d0da9477fe05aea58
戴扬杰 <1318610785@qq.com>
2019-04-13 15:47:31 +0800

    回复接口

--- server/article.js
+++ server/article.js
@@ -35,25 +35,16 @@ Router.get('/getToken',function(req,res){
     }
 })
 
-// Router.get('/getAllArticle',function(req,res){
-//     Article.find({},function(err,doc){
-//         if (err) {
-//             return res.json({code:1,msg:'获取文章列表失败'})
-//         }
-//         return res.json({code:0,list:doc,msg:'获取文章列表成功'})
-//     })
-   
-// })
-
-Router.get('/getAllMShare',function(req,res){
-    mShare.find({},function(err,doc){
+Router.get('/getAllArticle',function(req,res){
+    Article.find({},function(err,doc){
         if (err) {
-            return res.json({code:1,msg:'获取小程序首页数据成功'})
+            return res.json({code:1,msg:'获取文章列表失败'})
         }
-        return res.json({code:0,list:doc,msg:'获取小程序首页数据失败'})
+        return res.json({code:0,list:doc,msg:'获取文章列表成功'})
     })
    
 })
+
 
 
 module.exports = Router
--- server/mini.js
+++ server/mini.js
@@ -6,25 +6,24 @@ const mShare = model.getModel('mshare')
 
 Router.post('/uploadMShare',function(req,res){
     const {title,content,date,imagesUrl,time,location} = req.body
-    console.log(req.body)
-    // const mShareModel = new mShare({title,content,date,imagesUrl,time,location})
-    // mShareModel.save(function(e,d){
-    //     if(e){
-    //         return res.json({code:1,msg:'后端出错'})
-    //     }
-    //     return res.json({code:0,msg:'success'})
-    // })
+    const mShareModel = new mShare({title,content,date,imagesUrl,time,location})
+    mShareModel.save(function(e,d){
+        if(e){
+            return res.json({code:1,msg:'后端出错'})
+        }
+        return res.json({code:0,msg:'success'})
+    })
    
 })
 
-// Router.get('/getAllMShare',function(req,res){
-//     mShare.find({},function(err,doc){
-//         if (err) {
-//             return res.json({code:1,msg:'获取小程序首页数据成功'})
-//         }
-//         return res.json({code:0,list:doc,msg:'获取小程序首页数据失败'})
-//     })
+Router.get('/getAllMShare',function(req,res){
+    mShare.find({},function(err,doc){
+        if (err) {
+            return res.json({code:1,msg:'获取小程序首页数据成功'})
+        }
+        return res.json({code:0,list:doc,msg:'获取小程序首页数据失败'})
+    })
    
-// })
+})
 
 module.exports = Router
