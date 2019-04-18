2f64d60474bb3557728b18d7fe03f8475745ec52
戴扬杰 <1318610785@qq.com>
2019-04-18 17:58:29 +0800

    update

--- server/mini.js
+++ server/mini.js
@@ -28,21 +28,27 @@ Router.get('/getAllMShare',function(req,res){
 })
 
 Router.post('/getAlbumList',async function(req,res){
-    const {albumName} = req.body
-    console.log(req.body)
-
+    const albumName = req.body
+  
     var accessKey = 'VXe87vEB8q4ProaK5hQSlGEoJKsWWKYc2UwlMJ4Y';
     var secretKey = 'ZdVX2B9-sq1DzqUhH56XQrqhHerMC4OECRl8_V5q';
     var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
     var config = new qiniu.conf.Config();
     var bucketManager = new qiniu.rs.BucketManager(mac, config);
     var bucket = 'wwtfile';
-    var options = {
-        limit: 100,
-        prefix: 'album/',
-    };
 
-    await bucketManager.listPrefix(bucket, options, function(err, respBody, respInfo) {
+    let imgArray = []
+
+
+    for(let i=0;i<albumName.length;i++){
+      imgArray.push([])
+
+      var options = {
+        limit: 100,
+        prefix: 'album/'+albumName[i]+'/',
+      };
+
+      await bucketManager.listPrefix(bucket, options, function(err, respBody, respInfo) {
         if (err) {
           console.log(err);
           return res.json({code:1,data:err,msg:'获取相册列表失败'})
@@ -57,31 +63,28 @@ Router.post('/getAlbumList',async function(req,res){
         //   console.log(commonPrefixes);
           var items = respBody.items;
           
-          let imgArray = []
-          items.forEach(function(item) {
-            imgArray.push("http://cdn.mitty.work/"+item.key)
+          console.log(i)
+          items.forEach(function(item,idx) {
+            imgArray[i].push("http://cdn.mitty.work/"+item.key)
+            // console.log(imgArray)
             // console.log(item.putTime);
             // console.log(item.hash);
             // console.log(item.fsize);
             // console.log(item.mimeType);
             // console.log(item.endUser);
             // console.log(item.type);
+            
           });
-          return res.json({code:0,data:imgArray,msg:'获取相册列表成功'})
+
+          if(i==2){
+            return res.json({code:0,data:imgArray,msg:'获取相册列表成功'})
+          }
         } else {
           console.log(respInfo.statusCode);
           console.log(respBody);
         }
       });
-
-
-    // mShare.find({},function(err,doc){
-    //     if (err) {
-    //         return res.json({code:1,msg:'获取小程序首页数据失败'})
-    //     }
-    //     return res.json({code:0,list:doc,msg:'获取小程序首页数据成功'})
-    // })
-   
+    }
 })
 
 module.exports = Router
